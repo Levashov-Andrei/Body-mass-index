d065d2c7d0d7dae98a1691fb948933a732bb1447
Levashov Andrey <levashov.andrei2018@gmail.com>
2023-06-05 22:50:18 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        double height = 1.87; // Рост
+        int weight = 98; // Вес
+        double a = service.calculate(height,weight);
+
+        System.out.println("Рост "+height+"м");
+        System.out.println("Вес "+weight+"кг");
+        System.out.println();
+        System.out.println("Индекс массы тела "+ a);
+    }
+}
